9eb9d0bb8893257e6692eae418404496772e1974
Nuribe92 <nestoruribe92@hotmail.com>
2023-03-16 09:37:14 -0500

    arr.find()

--- aprendiendo-js/script.js
+++ aprendiendo-js/script.js
@@ -1,4 +1,7 @@
 /* para acceder a un array multidemencional podemos hacerlo de la siguiente form
 //  consolo.log(arr[0][1])
-//aqui no dice que vamos a mostrar el primer elemento del primer array seguido del 2do elemento del 2 array.*/
+//aqui nos dice que vamos a mostrar el primer elemento del primer array seguido del 2do elemento del 2 array.*/
+
+/*Busquedas dentro de un array
+arr.find(function(parametro)) */
 

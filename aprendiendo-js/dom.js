c34bf51529ca70575c54a689edd0d4908eeef506
Nuribe92 <nestoruribe92@hotmail.com>
2023-03-22 09:23:37 -0500

    document.getElementByTagName

--- aprendiendo-js/dom.js
+++ aprendiendo-js/dom.js
@@ -1,3 +1,15 @@
 let caja = document.getElementById("micaja");
 caja.innerHTML = "Texto en la caja desde JS";
 console.log(caja)
+
+
+let todosLosDivs = document.getElementsByTagName('div');
+
+todosLosDivs.array.forEach((valor, indice) => {
+    let parrafo = document.createElement("p");
+    let texto = document.createElement(valor);
+    parrafo.appendChild(texto);
+    
+});
+
+console.log(todosLosDivs)

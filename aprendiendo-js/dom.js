650a62334df014e1fc87aa3f8e3b5a88ea70a7d9
Nuribe92 <nestoruribe92@hotmail.com>
2023-03-25 09:42:42 -0500

    document.getelementbyclassname

--- aprendiendo-js/dom.js
+++ aprendiendo-js/dom.js
@@ -3,13 +3,21 @@ caja.innerHTML = "Texto en la caja desde JS";
 console.log(caja)
 
 
-let todosLosDivs = document.getElementsByTagName('div');
+//let todosLosDivs = document.getElementsByTagName('div');
 
-todosLosDivs.array.forEach((valor, indice) => {
-    let parrafo = document.createElement("p");
+//todosLosDivs.array.forEach((valor, indice) => {
+   /* let parrafo = document.createElement("p");
     let texto = document.createElement(valor);
     parrafo.appendChild(texto);
     
-});
+});*/
 
-console.log(todosLosDivs)
+
+let element;
+let divRojos = document.getElementsByClassName('rojo');
+for (element in divRojos){
+    if(divRojos[element].className == "rojo"){
+    divRojos[element].style.background = "red";
+    console.log(divRojos)
+    }
+}
